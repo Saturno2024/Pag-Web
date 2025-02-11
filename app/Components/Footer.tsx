6be0fa9f98288623ad7dc91808f4b38f1f545bbc
Lucas <lubaygorrea@gmail.com>
2025-02-11 20:38:11 -0300

    Actualizando cambios para Vercel

---
+++ app/Components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+      <footer>
+        <p>&copy; 2025 Mi Sitio Web. Todos los derechos reservados.</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
---
+++ app/Components/Navbar.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">Inicio</Link>
+        </li>
+        <li>
+          <Link href="/about">Acerca de</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contacto</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
